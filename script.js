df5a628bb3945b816c1ad4fb3e282213e47efdaf
camila <mariacamilaluango@gmail.com>
2025-02-07 00:03:40 -0500

    primer if

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ if (num.length < 2){
     console.log("la lista debe tener al menos dos numeros");
     return;
 }
+
+for (let i = 1; i < num.length; i++){
+    if(num[i] > num[i - 1]){
+        console.log(num[i]," es mayor que ", num[i - 1])
+    }
+}
 }
