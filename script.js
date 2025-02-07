fe002804a96c119a7326c2eeb1c0bab19bb12b9b
camila <mariacamilaluango@gmail.com>
2025-02-07 00:07:26 -0500

    else

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ for (let i = 1; i < num.length; i++){
         console.log(num[i]," es mayor que ", num[i - 1]);
     } else if (num[i] < num[i - 1]){
         console.log(num[i]," es menor que ", num[i - 1])
+    } else {
+        console.log(num[i]," es igual a ", num[i - 1])
     }
 }
 }
